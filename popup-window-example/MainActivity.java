6075339c43607c6c2f7dfb618745b022830b6cc1
bakoalex <bakoalex9595@gmail.com>
2016-09-04 19:47:46 +0200

    Create MainActivity.java

---
+++ popup-window-example/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.bakoa.layouttest;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.PopupWindowCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupWindow;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnShow = (Button) findViewById(R.id.btnShowPopup);
+    }
+
+    public void onClick(View v) {
+        if (v.getId()==R.id.btnShowPopup) {
+            LayoutInflater layoutInflater = (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+            View popupView = layoutInflater.inflate(R.layout.popup, null);
+            final PopupWindow popupWindow = new PopupWindow(popupView, DrawerLayout.LayoutParams.WRAP_CONTENT, DrawerLayout.LayoutParams.WRAP_CONTENT);
+
+            Button btnDismiss = (Button) popupView.findViewById(R.id.btnPopUpOk);
+            btnDismiss.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    popupWindow.dismiss();
+                }
+            });
+
+            popupWindow.showAsDropDown(btnShow, 50, -30);
+        }
+    }
+
+
+}
